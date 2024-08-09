84e2c8658e1597bbafab36f4bd4628ea23bc09ee
K Dibakar Patro <80563512+dibakarpatro@users.noreply.github.com>
2024-08-09 16:14:30 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "thisismyprojectcontainer900" # Replace with your actual S3 bucket name
+    bucket = "thisismyprojectcontainer9000" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
